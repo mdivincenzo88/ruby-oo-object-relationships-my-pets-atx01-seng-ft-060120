181d629d20be4bf14bbc2bc0b3012d54ce54af5b
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-05 21:37:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
   # code goes here
+  attr_reader :name 
+  attr_accessor :owner, :mood
+  
+  @@all = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self 
+    end
+def self.all
+  @@all
+end
+
+def cats
+  cats.all 
+
+end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all= []
+  def initialize(name) 
+    @name = name
+    @species = "human"
+    self.class.all << self
+  end
+  
+  def say_species
+    "I am a #{@species}."
+    end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats 
+Cat.all.select do |cats|
+  cats.owner == self
+end
+end
+
+
 end
